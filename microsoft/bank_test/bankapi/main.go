ce4f1edea502dfcbaa231f39156e2ce6b280ef41
Makoto Yoshida <milmed1013@gmail.com>
2022-02-02 23:58:35 +0900

    add test

---
+++ microsoft/bank_test/bankapi/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	bank "go-tutorial/microsoft/bank_test/bankcore"
+)
+
+func main() {
+	fmt.Println(bank.Hello())
+}
---
+++ microsoft/bank_test/go.mod
@@ -0,0 +1,3 @@
+module go-tutorial/microsoft/bank_test
+
+go 1.17
